312965b354ecfada875827f09972d039a35bec96
oshjma <k.oshjma@gmail.com>
2016-06-09 00:22:06 +0900

    Modify keybind on dired-mode

--- .emacs.d/inits/10_dired.el
+++ .emacs.d/inits/10_dired.el
@@ -1,5 +1,6 @@
 (add-hook 'dired-load-hook
   (lambda ()
+    (define-key dired-mode-map (kbd "i") nil)
     (define-key dired-mode-map (kbd "E") 'wdired-change-to-wdired-mode)
     (define-key dired-mode-map (kbd ".") 'dired-up-directory)
     (define-key dired-mode-map (kbd "C-.") 'dired-up-directory)))
