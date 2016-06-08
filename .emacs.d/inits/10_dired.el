b6eacbea847ddf1a0e3ac524377c301ebe141bd0
oshjma <k.oshjma@gmail.com>
2016-06-09 00:13:58 +0900

    Remove "total used..." lines from Dired buffer

--- .emacs.d/inits/10_dired.el
+++ .emacs.d/inits/10_dired.el
@@ -3,3 +3,11 @@
     (define-key dired-mode-map (kbd "E") 'wdired-change-to-wdired-mode)
     (define-key dired-mode-map (kbd ".") 'dired-up-directory)
     (define-key dired-mode-map (kbd "C-.") 'dired-up-directory)))
+
+(add-hook 'dired-after-readin-hook
+  (lambda ()
+    (let ((buffer-read-only nil))
+      (save-excursion
+        (goto-char (point-min))
+        (flush-lines "^ *total")
+        (set-buffer-modified-p nil)))))
