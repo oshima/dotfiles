72feeeec364b6f6b14a1fc136e74231bfde1e62c
oshjma <k.oshjma@gmail.com>
2016-10-11 23:23:54 +0900

    Customize mode line

--- .emacs.d/inits/10_mode-line.el
+++ .emacs.d/inits/10_mode-line.el
@@ -1,25 +1,16 @@
+;; http://emacs.stackexchange.com/a/7542
+(defun simple-mode-line-render (left right)
+  (let ((center-width
+         (- (window-total-width)
+            (length (format-mode-line (append left right))))))
+    (append left (make-list center-width " ") right)))
+
 (setq-default mode-line-format
-              '("%e"
-                mode-line-mule-info
-                mode-line-modified
-                mode-line-remote
-                "  "
-                mode-line-buffer-identification
-                "  "
-                mode-line-position
-                "  "
-                (vc-mode vc-mode)
-                mode-line-modes))
-
-(setq-default mode-line-buffer-identification
-              '(:eval (if (buffer-modified-p)
-                          (propertize "%17b" 'face 'warning)
-                        (propertize "%17b" 'face 'mode-line-buffer-id))))
-
-(setq mode-line-position
-      '(:eval (format "%%l/%d" (count-lines (point-max) (point-min)))))
-
-;; http://emacs.stackexchange.com/questions/10955
-(setcdr (assq 'vc-mode mode-line-format)
-        '((:eval (cond ((string-match "^ Git." vc-mode) (concat (substring vc-mode 5) "  "))
-                       (t "")))))
+              '(:eval (simple-mode-line-render
+                       '("[%+]"
+                         " "
+                         mode-line-buffer-identification)
+                       '("%l,%c"
+                         (vc-mode vc-mode)
+                         " "
+                         mode-name))))
