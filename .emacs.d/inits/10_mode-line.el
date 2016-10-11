5393d8d2e035e179c3f56e09ebca48b7ae19a29a
oshjma <k.oshjma@gmail.com>
2016-10-11 23:52:30 +0900

    Refactor

--- .emacs.d/inits/10_mode-line.el
+++ .emacs.d/inits/10_mode-line.el
@@ -1,16 +1,21 @@
 ;; http://emacs.stackexchange.com/a/7542
-(defun simple-mode-line-render (left right)
+(defun make-mode-line-format (left right)
   (let ((center-width
          (- (window-total-width)
             (length (format-mode-line (append left right))))))
     (append left (make-list center-width " ") right)))
 
+(setq mode-line-left-format
+      '("[%+]"
+        " "
+        mode-line-buffer-identification))
+
+(setq mode-line-right-format
+      '("%l,%c"
+        (vc-mode vc-mode)
+        " "
+        mode-name))
+
 (setq-default mode-line-format
-              '(:eval (simple-mode-line-render
-                       '("[%+]"
-                         " "
-                         mode-line-buffer-identification)
-                       '("%l,%c"
-                         (vc-mode vc-mode)
-                         " "
-                         mode-name))))
+              '(:eval (make-mode-line-format mode-line-left-format
+                                             mode-line-right-format)))
