41e87ea6d933ea7d1e671c233fc8a70117bfe9de
oshjma <k.oshjma@gmail.com>
2017-05-07 04:38:38 +0900

    Change face of `true`, `false` and `nil` in ruby-mode

--- .emacs.d/inits/40_ruby-mode.el
+++ .emacs.d/inits/40_ruby-mode.el
@@ -1,4 +1,11 @@
 (add-to-list 'auto-mode-alist '("/\\.pryrc\\'" . ruby-mode))
 
 (with-eval-after-load 'ruby-mode
-  (setq ruby-insert-encoding-magic-comment nil))
+  (setq ruby-insert-encoding-magic-comment nil)
+
+  (font-lock-add-keywords
+   'ruby-mode
+   `((,(rx symbol-start
+           (or "true" "false" "nil")
+           symbol-end)
+      0 font-lock-variable-name-face))))
