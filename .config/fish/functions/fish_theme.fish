f877c31d70478a5981e07bcb198632ae84e72b5e
oshjma <k.oshjma@gmail.com>
2017-05-07 04:35:09 +0900

    Customize fish theme

--- .config/fish/functions/fish_theme.fish
+++ .config/fish/functions/fish_theme.fish
@@ -4,7 +4,7 @@ function fish_theme
   set -g fish_color_comment 949494
   set -g fish_color_cwd normal
   set -g fish_color_cwd_root normal
-  set -g fish_color_end magenta
+  set -g fish_color_end normal
   set -g fish_color_error normal
   set -g fish_color_escape magenta
   set -g fish_color_history_current normal
@@ -14,7 +14,7 @@ function fish_theme
   set -g fish_color_operator d7875f
   set -g fish_color_param normal
   set -g fish_color_quote green
-  set -g fish_color_redirection magenta
+  set -g fish_color_redirection normal
   set -g fish_color_search_match --background=444444
   set -g fish_color_selection normal
   set -g fish_color_user normal
