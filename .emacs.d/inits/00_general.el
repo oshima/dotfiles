f1652490a459763b6763079a0a563a31d9b1d27c
oshjma <k.oshjma@gmail.com>
2018-09-30 03:26:23 +0900

    Set tab width to 4 spaces

--- .emacs.d/inits/00_general.el
+++ .emacs.d/inits/00_general.el
@@ -8,9 +8,9 @@
 (setq-default truncate-lines t)
 (setq-default truncate-partial-width-windows t)
 
-;; Tab is 2 spaces
+;; Tab width is 4 spaces
 (setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
+(setq-default tab-width 4)
 
 ;; Don't create these files automatically
 (setq make-backup-files nil)
