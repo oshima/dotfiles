a50dfdc145fa60511e1efd251c8d84f019e43f40
oshjma <k.oshjma@gmail.com>
2018-11-20 23:13:27 +0900

    trivial changes

--- .emacs.d/inits/00_theme.el
+++ .emacs.d/inits/00_theme.el
@@ -1,10 +1,3 @@
-(defun custom-theme-set-faces-simply (theme &rest args)
-  (apply #'custom-theme-set-faces
-         theme
-         (mapcar #'(lambda (arg)
-                     `(,(car arg) ((t (,@(cdr arg))))))
-                 args)))
-
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 (load-theme 'my t)
