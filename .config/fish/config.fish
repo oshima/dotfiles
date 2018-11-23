23254697439be64146d16e927818799d53e535cc
oshjma <k.oshjma@gmail.com>
2018-11-23 23:03:16 +0900

    Change config for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 set -x SHELL (which fish)
-set -x PATH ~/.cargo/bin $PATH
+
+set ruby_bin /usr/local/opt/ruby/bin
+set gem_bin /usr/local/lib/ruby/gems/2.5.0/bin
+set -x PATH $ruby_bin $gem_bin $PATH
 
 fish_theme
 fzf_config
-rbenv init - --no-rehash | source
